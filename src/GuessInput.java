ad36eadc10fde9226fc501337c61afb98f5c6638
BT_1N3_32@BKTECH.LOCAL <BT_1N3_32@BKTECH.LOCAL>
2018-10-11 14:44:40 -0400

    Initial commit

---
+++ src/GuessInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GuessInput {
+    public static void main(String[] args) {
+        double x = Math.random();
+        int num = (int) (x * 100 + 1);
+        int count = 0;
+
+        while (int guess != num); {
+            Scanner = new Scanner(System.in);
+            System.out.println("Chose a random number.");
+            guess = reader.nextInt();
+    }
+}
+
+
+
